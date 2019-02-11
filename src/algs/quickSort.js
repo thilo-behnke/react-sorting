d705682d66bcc5501ff7499692db3c2da89a7e76
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-02-11 16:24:45 +0100

    ejected from create-react-app to add babel plugin for tail recursion

--- src/algs/quickSort.js
+++ src/algs/quickSort.js
@@ -1,8 +1,7 @@
 export const quickSort = list => {
-   const pivot = list && list.length ? list[0] : undefined;
+   const [pivot, ...tail] = list;
 
    if (pivot !== undefined) {
-      const [head, ...tail] = list;
       const smaller = tail.filter(a => a < pivot);
       const bigger = tail.filter(a => a >= pivot);
       return [...quickSort(smaller), pivot, ...quickSort(bigger)].flat();
